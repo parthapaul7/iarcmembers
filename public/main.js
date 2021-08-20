3363bda8c7539f10d67e3f7d7a5094e3aabfeb72
partha <pparthapaul11@gmail.com>
2021-08-20 21:51:19 +0530

    api change

--- public/main.js
+++ public/main.js
@@ -1,62 +1,45 @@
 const secCon = document.getElementById("sCotainer");
 const memCon = document.getElementById("mContainer");
 
-let names=[];
-
+let names = [];
 
 //firebase cutiyapa start
 
-
 let storageRef = firebase.storage().ref();
 
-
-  storageRef.child('images/').listAll().then((result) => {
-    let x= document.getElementsByClassName("cardImg");
+storageRef
+  .child("images/")
+  .listAll()
+  .then((result) => {
+    let x = document.getElementsByClassName("cardImg");
     result.items.forEach((element) => {
-      element.getDownloadURL().then(function(url){
-        // console.log(url);
-        
+      element.getDownloadURL().then(function (url) {
         console.log(url);
-        for(let i=0;i<names.length;i++){
-
-        if(url.includes(names[i].slice(0,4))){
-          console.log("yes",names[i].slice(0,5));
-          
-        x[i].innerHTML= `<img src="${url}".jpeg" class="card-img-top" alt="..." >`
+        for (let i = 0; i < names.length; i++) {
+          if (url.includes(names[i].slice(0, 4))) {
+            x[
+              i
+            ].innerHTML = `<img src="${url}".jpeg" class="card-img-top" alt="..." >`;
+          }
         }
-        }
-  
-      })
-      console.log("before printing");
-     
+      });
     });
-    
-  
-    
-  })
+  });
 
-  // firebase end end
-
-
-
+// firebase end end
 
 let secretary = "";
 let members = "";
 
 function getData() {
-  let count = 0;
-  url = "https://sheet.best/api/sheets/ca077534-54f7-42a7-ad9c-ac1668215f96";
+  url = "https://sheetdb.io/api/v1/iti4m6gu0rspr";
   fetch(url, { method: "get" })
     .then((response) => {
       return response.json();
     })
     .then((info) => {
-      info.forEach((e,index) => {
-        
-
+      info.forEach((e, index) => {
         if (e.ROLE.includes("Secretary")) {
-          console.log(index);
-      
           secretary += ` <div class="card" style="width: 20rem;">
             <div class="cardImg">
             <img src="images/loading.gif".jpeg" class="card-img-top" alt="..." ></div>
@@ -84,9 +67,3 @@ function getData() {
 }
 
 getData();
-
-
-
-
-
-
